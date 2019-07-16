f6d1890874aab172732db246c7490aaa20bb2d0b
japes930 <jpvillaflores9@gmail.com>
2019-07-16 16:12:14 -0400

    First commit

---
+++ src/main/java/com/example/springboot_204ex/Springboot204exApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_204ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot204exApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot204exApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springboot_204ex/Springboot204exApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springboot_204ex;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot204exApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
